0dd3184950b98dab53f0f9a7b96fafa4fb03e0a0
Nicola Deidda <nicola.deidda@icloud.com>
2024-02-02 09:08:39 +0100

    Bug fixes

--- iot-webapp/src/views/LogoutView.vue
+++ iot-webapp/src/views/LogoutView.vue
@@ -2,9 +2,13 @@
 import { removeToken } from '@/assets/js/token';
 import router from '@/router';
 import { onMounted } from 'vue';
+import doLogout from '@/assets/api/doLogout';
 
-onMounted(() => {
-    removeToken();
-    router.push({ path: '/' });
+onMounted(async () => {
+    const response = await doLogout();
+    if (response) {
+        removeToken();
+        router.push({ path: '/' });
+    }
 });
 </script>
